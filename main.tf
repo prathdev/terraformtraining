26f3e80ae4f1d1e634701562e67fa00cc96d1db2
Pratheeksha Devegowda <pratheeksha.dg@gmail.com>
2017-04-12 10:04:24 -0700

    updating main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-#
-# DO NOT DELETE THESE LINES!
-#
-# Your AMI ID is:
-#
-#     ami-eea9f38e
-#
-# Your subnet ID is:
-#
-#     subnet-7e08481a
-#
-# Your security group ID is:
-#
-#     sg-834d35e4
-#
-# Your Identity is:
-#
-#     autodesk-eagle
-#
 variable "aws_access_key" {
   type = "string"
 }
@@ -42,13 +23,9 @@ terraform {
 }
 
 provider "aws" {
-  #access_key = "AKIAIRLDFGIXJJHLQB6A"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
-
-  #secret_key = "xxsrCpcCMNl7IYU/+SHmJL4DOlx6yJc3nCZoHH6W"
-  #region     = "us-west-1"
 }
 
 resource "aws_instance" "web" {
